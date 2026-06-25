be943f6f2bed86994ef4c55145407ec5b11d72c7
wendiele <wendelltorgaict123@gmail.com>
2026-06-26 07:49:45 +0800

    Update gui.lua

--- gui.lua
+++ gui.lua
@@ -1,1 +1,16 @@
+local Settings = require(script.Parent.settings)
 
+local Gui = {}
+
+function Gui:Init(screenGui)
+    local frame = screenGui:WaitForChild("Frame")
+    local title = frame:WaitForChild("Title")
+
+    frame.Size = Settings.GUI.Size
+    frame.BackgroundColor3 = Settings.Theme.Background
+
+    title.Text = Settings.GUI.Name
+    title.TextColor3 = Settings.Theme.Text
+end
+
+return Gui
