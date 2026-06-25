5d3986196ac204d8378778c4c33e489f30d57c11
wendiele <wendelltorgaict123@gmail.com>
2026-06-26 07:32:43 +0800

    Update settings.lua

--- settings.lua
+++ settings.lua
@@ -1,1 +1,14 @@
+local Settings = {}
 
+Settings.Theme = {
+    Background = Color3.fromRGB(35,35,35),
+    Accent = Color3.fromRGB(0,170,255),
+    Text = Color3.new(1,1,1)
+}
+
+Settings.GUI = {
+    Name = "My Framework",
+    Size = UDim2.new(0,500,0,300)
+}
+
+return Settings
